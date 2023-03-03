866d58e9266c2d356d13a9ae8096108e6db6a22e
YYJ-f <ypow@eumworks.com>
2023-03-03 09:23:44 +0900

    Revert "."
    This reverts commit 9646e2ed20dbf2921eef837d00bceeca6c0319df.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify??<span className="fancy">.</span>
+            Hi, We're Next.js & Netlify<span className="fancy">.</span>
           </h1>
           <span className="handle">@nextjs-netlify-blog</span>
           <h2>A blog template with Next.js and Netlify.</h2>
